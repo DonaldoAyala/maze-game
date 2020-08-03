42351662946652a224b8747937415673c72c96a0
Donaldo Ayala <ayala.segoviano.donaldo@gmail.com>
2020-08-03 00:07:52 -0500

    Added maze generation algorithm (not random yet) and corrected some functions

--- PythonMaze/maze.py
+++ PythonMaze/maze.py
@@ -1,12 +1,14 @@
 import pygame
 
 COLORS = {
-	"WHITE" : (255,255,255),
-	"BLACK" : (0,0,0),
-	"RED" 	: (255,0,0),
-	"GREEN" : (0,255,0),
-	"BLUE"	: (0,0,255)
+	"WHITE" : (255, 255, 255),
+	"BLACK" : (0, 0, 0),
+	"RED" 	: (255, 0, 0),
+	"GREEN" : (0, 255, 0),
+	"BLUE"	: (0, 0, 255)
 }
+
+
 class Game:
 	def __init__(self):
 		self.screen = Screen(200, 200, COLORS["BLACK"], "Maze Game")
@@ -22,6 +24,7 @@ class Game:
 			self.screen.window.fill((COLORS["BLACK"]))
 			self.screen.draw_maze("hello")
 
+
 class Screen:
 	def __init__(self, width, height, color, caption):
 		self.width = width
@@ -30,22 +33,72 @@ class Screen:
 		self.caption = caption
 		self.window = pygame.display.set_mode((self.width, self.height))
 		pygame.display.set_caption(self.caption)
+
 	def draw_maze(self, maze):
-		pygame.draw.rect(self.window, COLORS["GREEN"], (10, 10, 10, 10))
-		pygame.display.update()
+		pass
+
 
 class Maze:
-	def __init__(self, screenWidth, screenHeight, cellSize):
-		self.width = screenWidth / cellSize
-		self.height = screenHeight / cellSize
-		self.cells = [[Cell()]*self.width  for i in range(self.height)]
+	def __init__(self, screen_width, screen_height, cell_size):
+		self.columns = screen_width / cell_size
+		self.rows = screen_height / cell_size
+		self.cells = [[Cell(0, 0)]*self.columns for i in range(self.rows)]
+		for i in range(self.columns):
+			for j in range(self.rows):
+				self.cells[i][j].x = i * cell_size
+				self.cells[i][j].y = j * cell_size
+
+	def visit_cell(self, x, y, move):
+		if x + move[0] < 0 or x + move[1] >= self.columns:
+			return False
+		if y + move[1] < 0 or y + move[1] >= self.rows:
+			return False
+		if self.cells[x + move[0]][y + move[1]].visited:
+			return False
+		top = [0, -1]
+		right = [1, 0]
+		bottom = [0, 1]
+		left = [-1, 0]
+		if move == top:
+			self.cells[x][y].remove_wall(0) # Top
+			self.cells[x + move[0]][y + move[1]].remove_wall(2) # Bottom
+		elif move == right:
+			self.cells[x][y].remove_wall(1) # Right
+			self.cells[x + move[0]][y + move[1]].remove_wall(3) # Left
+		elif move == bottom:
+			self.cells[x][y].remove_wall(2) # Bottom
+			self.cells[x + move[0]][y + move[1]].remove_wall(0) # Top
+		else:
+			self.cells[x][y].remove_wall(3) # Left
+			self.cells[x + move[0]][y + move[1]].remove_wall(1) # Right
+		return True
+
+	def generate(self):
+		current_cell = self.cells[0][0]
+		stack = []
+		# Moves are: top right bottom left
+		moves = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+		stack.append(current_cell)
+		while  not stack.empty():
+			current_cell = stack.top()
+			stack.pop()
+			for mvs in moves:
+				if self.visit_cell(current_cell.x, current_cell.y, mvs):
+					stack.append(self.cells[current_cell.x + mvs[0]][current_cell.y + mvs[1]])
+					self.cells[current_cell.x + mvs[0]][current_cell.y + mvs[1]].visited = True
+
 
 class Cell:
-	def __init__(self):
+	def __init__(self, x, y):
 		# Walls: Up , Right , Bottom , Left
-		self.walls = {True , True , True , True}
+		self.walls = {True, True, True, True}
+		self.x = x
+		self.y = y
+		self.visited = False
+
 	def remove_wall(self, wall):
 		self.walls[wall] = False
+
 
 if __name__ == "__main__":
 	game = Game()
