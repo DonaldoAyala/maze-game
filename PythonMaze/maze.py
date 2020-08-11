a2a2ff6be956e7d9dd50b697ffb93afb36ff50a4
Donaldo Ayala <ayala.segoviano.donaldo@gmail.com>
2020-08-11 01:42:40 -0500

    Fixed some of the bugs that ocurred when generating the maze (not updating the cells)

--- PythonMaze/maze.py
+++ PythonMaze/maze.py
@@ -40,94 +40,6 @@ class Screen:
 		pass
 
 
-class Maze:
-	moves = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-	def __init__(self, screen_width, screen_height, cell_size):
-		self.columns = round(screen_width / cell_size)
-		self.rows = round(screen_height / cell_size)
-		self.cells = [[Cell(int(0), int(0))] * self.columns for i in range(self.rows)]
-		for i in range(self.columns):
-			for j in range(self.rows):
-				self.cells[i][j].x = i * cell_size
-				self.cells[i][j].y = j * cell_size
-
-	def is_valid(self, cell):
-		if cell.x < 0 or cell.x >= self.columns:
-			return False
-		if cell.y < 0 or cell.y >= self.rows:
-			return False
-		if self.cells[cell.x][cell.y].visited:
-			return False
-		return True
-
-	def has_neighbours(self, cell):
-		for mv in self.moves:
-			if self.is_valid(Cell(cell.x + mv[0], cell.y + mv[1])) :
-				return True
-			return False
-
-	def visit_cell(self, cell, move):
-		if not self.is_valid(Cell(cell.x + move[0], cell.y + move[1])):
-			return False
-		if move == self.moves[0]:
-			self.cells[cell.x][cell.y].remove_wall(0) # Top
-			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(2) # Bottom
-		elif move == self.moves[1]:
-			self.cells[cell.x][cell.y].remove_wall(1) # Right
-			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(3) # Left
-		elif move == self.moves[2]:
-			self.cells[cell.x][cell.y].remove_wall(2) # Bottom
-			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(0) # Top
-		else:
-			self.cells[cell.x][cell.y].remove_wall(3) # Left
-			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(1) # Right
-		self.cells[cell.x + move[0]][cell.y + move[1]].visited = True
-		return True
-
-	def get_random_neighbour(self, cell):
-		possible_moves = []
-		for i in range(0, 4):
-			if self.is_valid(Cell(cell.x + self.moves[i][0], cell.y + self.moves[i][1])):
-				possible_moves.append(i)
-		r = random.randint(0, lxen(possible_moves))
-		if len(possible_moves):
-			return possible_moves[r]
-		return -1
-
-	def generate(self):
-		current_cell = self.cells[0][0]
-		stack = [current_cell]
-		while len(stack):
-			current_cell = stack[-1]
-			stack.pop()
-			if self.has_neighbours(current_cell):
-				stack.append(current_cell)
-			print(len(stack))
-			index = self.get_random_neighbour(current_cell)
-			if index != -1:
-				self.visit_cell(current_cell.x, current_cell.y, self.moves[index])
-				stack.append(self.cells[current_cell.x + self.moves[index][0]][current_cell.y + self.moves[index][1]])
-			for i in range(self.columns):
-				for j in range(self.rows):
-					print(self.cells[i][j], end=' || ')
-				print(" ")
-				print("----------------------------------------------------")
-
-
-		"""
-		for i in self.cells:
-			for j in i:
-				for k in range(4):
-					if j.walls[k]:
-						print(k, end=' ')
-					else:
-						print("_", end=' ')
-				print(" || ", end=' ')
-			print(" ")
-			print("---------------------------------------------------------------")
-		"""
-
-
 class Cell:
 	def __init__(self, x, y):
 		# Walls: Up , Right , Bottom , Left
@@ -150,6 +62,102 @@ class Cell:
 		if self.walls[3]:
 			string += 'l '
 		return string
+
+
+class Maze:
+	moves = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+	def __init__(self, screen_width, screen_height, cell_size):
+		self.columns = round(screen_width / cell_size)
+		self.rows = round(screen_height / cell_size)
+		self.cells = [[0] * self.columns for i in range(self.rows)]
+		for i in range(0,self.columns):
+			for j in range(0,self.rows):
+				self.cells[i][j] = Cell(i * cell_size,j * cell_size)
+
+	def is_valid(self, cell):
+		print(cell.x," ",cell.y)
+		if cell.x < 0 or cell.x >= self.columns:
+			return False
+		if cell.y < 0 or cell.y >= self.rows:
+			return False
+		if self.cells[cell.x][cell.y].visited:
+			return False
+		return True
+
+	def has_neighbours(self, cell):
+		for mv in self.moves:
+			if self.is_valid(Cell(cell.x + mv[0], cell.y + mv[1])):
+				return True
+		return False
+
+	def visit_cell(self, cell, move):
+		if not self.is_valid(Cell(cell.x + move[0], cell.y + move[1])):
+			return False
+		if move == self.moves[0]:
+			self.cells[cell.x][cell.y].remove_wall(0) # Top
+			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(2) # Bottom
+		elif move == self.moves[1]:
+			self.cells[cell.x][cell.y].remove_wall(1) # Right
+			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(3) # Left
+		elif move == self.moves[2]:
+			self.cells[cell.x][cell.y].remove_wall(2) # Bottom
+			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(0) # Top
+		else:
+			self.cells[cell.x][cell.y].remove_wall(3) # Left
+			self.cells[cell.x + move[0]][cell.y + move[1]].remove_wall(1) # Right
+		self.cells[cell.x + move[0]][cell.y + move[1]].visited = True
+		return True
+
+	def get_random_neighbour(self, cell):
+		possible_moves = []
+		for i in range(0, 4):
+
+			print(self.is_valid(Cell(cell.x + self.moves[i][0], cell.y + self.moves[i][1])))
+			if self.is_valid(Cell(cell.x + self.moves[i][0], cell.y + self.moves[i][1])):
+				possible_moves.append(i)
+		r = random.randint(0, len(possible_moves))
+		if len(possible_moves):
+			return possible_moves[r]
+		return -1
+
+	def generate(self):
+		print(self.get_random_neighbour(self.cells[1][1]))
+		"""
+		current_cell = self.cells[0][0]
+		stack = [current_cell]
+		
+		while len(stack):
+			current_cell = stack[-1]
+			stack.pop()
+			if self.has_neighbours(current_cell):
+				stack.append(current_cell)
+			index = self.get_random_neighbour(current_cell)
+			print(len(stack))
+			print(index)
+			if index != -1:
+				self.visit_cell(current_cell, self.moves[index])
+				stack.append(self.cells[current_cell.x + self.moves[index][0]][current_cell.y + self.moves[index][1]])
+			for i in range(self.columns):
+				for j in range(self.rows):
+					print(self.cells[i][j], end=' || ')
+				print(" ")
+				print("----------------------------------------------------")
+		"""
+		"""
+		for i in self.cells:
+			for j in i:
+				for k in range(4):
+					if j.walls[k]:
+						print(k, end=' ')
+					else:
+						print("_", end=' ')
+				print(" || ", end=' ')
+			print(" ")
+			print("---------------------------------------------------------------")
+		"""
+
+
+
 
 if __name__ == "__main__":
 	maze = Maze(int(100), int(100), int(20))
