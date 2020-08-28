740d3fe4948dbcff520894b93a71b55b4120f342
Donaldo Ayala <ayala.segoviano.donaldo@gmail.com>
2020-08-28 01:45:15 -0500

    Updated menu class, added text and highest score to menu screen

--- PythonMaze/Menu.py
+++ PythonMaze/Menu.py
@@ -1,25 +1,38 @@
-import pygame
-import random
 import Color as color
+from Scoreboard import *
 
 
 class Menu:
 	def __init__(self):
+		sco_boa = Scoreboard()
+		self.texts = []
+		self.texts.append(ScreenText("PyMaze", (175, 40), color.green, 50))
+		self.texts.append(ScreenText("Highest Score", (165, 100), color.green, 30))
+		self.texts.append(ScreenText(str(sco_boa.get_highest_score()), (235, 140), color.yellow, 30))
 		self.buttons = []
 		# 'Easy difficulty button'
-		self.buttons.append(Button((50,300),(100,30),(color.gray),"Easy"))
+		self.buttons.append(Button((50, 210), (100, 30), color.gray, "Easy", color.black))
 		# 'Medium difficulty button'
-		self.buttons.append(Button((200,300),(100,30),(color.gray),"Medium"))
+		self.buttons.append(Button((200, 210), (100, 30), color.gray, "Medium", color.black))
 		# 'Hard difficulty button'
-		self.buttons.append(Button((350,300),(100,30),(color.gray),"Hard"))
+		self.buttons.append(Button((350, 210), (100, 30), color.gray, "Hard", color.black))
+
+
+class ScreenText:
+	def __init__(self, text, position, color, font_size):
+		self.text = text
+		self.position = position
+		self.color = color
+		self.font_size = font_size
 
 class Button:
 
-	def __init__(self, position, size, color, text):
+	def __init__(self, position, size, color, text, text_color):
 		self.position = position
 		self.size = size
 		self.color = color
 		self.text = text
+		self.text_color = text_color
 
 	def change_color(self, color):
 		self.color = color
